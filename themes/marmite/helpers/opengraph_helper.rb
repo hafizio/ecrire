0a965be47a01a838143dff1df0fed6706cca766a
Pier-Olivier Thibault <pothibo@gmail.com>
2014-02-11 11:55:31 -0500

    Fixed Open Graph warnings

--- themes/marmite/helpers/opengraph_helper.rb
+++ themes/marmite/helpers/opengraph_helper.rb
@@ -6,7 +6,9 @@ module OpengraphHelper
       og_title,
       og_type('article'),
       og_image(post),
-      content_tag(:meta, nil, property: 'article:published_time', content: post.published_at.iso8601)
+      content_tag(:meta, nil, property: 'article:published_time', content: post.published_at.iso8601),
+      content_tag(:meta, nil, property: 'og:url', content: request.url),
+      content_tag(:meta, nil, property: 'og:description', content: Rails.configuration.meta.description)
     ].join.html_safe
   end
 
