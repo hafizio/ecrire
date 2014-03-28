66c7f41e5b56a730d05e5567444853aa267feb78
Pier-Olivier Thibault <pothibo@gmail.com>
2014-03-28 14:21:43 -0400

    Added form in the header to subscibe to the mailing list.

--- themes/pothibo/helpers/blog_helper.rb
+++ themes/pothibo/helpers/blog_helper.rb
@@ -5,7 +5,20 @@ module BlogHelper
     content_tag :nav, class: %w(site) do
       [
         image_tag('profile.jpg'),
-        link_to('Articles', :root)
+        link_to('Articles', :root),
+        content_tag(:div, nil, class: %w(spacer)),
+        mailing_list_form
+      ].join.html_safe
+    end
+  end
+
+  private
+
+  def mailing_list_form
+    form_tag "http://pothibo.us4.list-manage.com/subscribe/post?u=c1a7606ebf0d844b14ef03fdf&amp;id=a0ae11323b", method: "post", target: "_blank" do
+      [
+        email_field_tag(:EMAIL, nil, placeholder: "Subscribe to the mailing list"),
+        button_tag("Subscribe")
       ].join.html_safe
     end
   end
